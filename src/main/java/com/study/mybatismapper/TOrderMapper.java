dff7e1700b8caa3629b5a0cfe67b4b0e944ef1b9
wanghuihui <wanghuihui@daojia.com>
2018-07-30 17:18:36 +0800

    shading-jdbc 分库、分表、读写分离

---
+++ src/main/java/com/study/mybatismapper/TOrderMapper.java
@@ -0,0 +1,24 @@
+package com.study.mybatismapper;
+
+
+import com.study.pojo.TOrder;
+import com.study.pojo.TOrderExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TOrderMapper {
+    int countByExample(TOrderExample example);
+
+    int deleteByExample(TOrderExample example);
+
+    int insert(TOrder record);
+
+    int insertSelective(TOrder record);
+
+    List<TOrder> selectByExample(TOrderExample example);
+
+    int updateByExampleSelective(@Param("record") TOrder record, @Param("example") TOrderExample example);
+
+    int updateByExample(@Param("record") TOrder record, @Param("example") TOrderExample example);
+}
